764edfaeb9758abe731bc775924101b7ae212778
Tiberio Silva <est_tiberio@BM.REDERS>
2021-01-20 17:23:55 -0300

    Dice Exercise

---
+++ state-demo/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import RandomButton from './RandomButton.js';
+
+function App() {
+  return (
+      <div className="App">
+        <RandomButton />
+      </div>
+  );
+}
+
+export default App;
